0952bd7af4b7d510b83ebfde67e0abb1a0e0a9c3
waterfausett <waterfausett@gmail.com>
2020-07-03 00:55:49 -0500

    updated emojis ♥

--- app/message-handlers/command-message-handler.js
+++ app/message-handlers/command-message-handler.js
@@ -38,7 +38,7 @@ module.exports = {
                     .addFields(
                         { name: 'Definition', value: trim(answer.definition, 1024) },
                         { name: 'Example', value: trim(answer.example, 1024) },
-                        { name: 'Rating', value: `${answer.thumbs_up} 👍\n${answer.thumbs_down} 👎` }
+                        { name: 'Rating', value: `:star_struck: \`${answer.thumbs_up}\` :cry: \`${answer.thumbs_down}\`` }
                     );
     
                 return [embed];
