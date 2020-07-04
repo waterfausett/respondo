264806f0b5135dcfd9308b5953b7883a8210c563
waterfausett <waterfausett@gmail.com>
2020-07-03 21:18:25 -0500

    updated the logger formats

--- app/services/logger.service.js
+++ app/services/logger.service.js
@@ -5,14 +5,14 @@ const isProduction = (process.env.NODE_ENV === 'production');
 
 const logger = winston.createLogger({
     level: isProduction ? 'info' : 'debug',
+    format: format.combine(
+        isProduction ? format.uncolorize() : format.colorize({ all: true }),
+        format.align(),
+        format.errors({ stack: true }),
+        format.simple(),
+    ),
     transports: [
-        new transports.Console({
-            format: format.combine(
-                isProduction ? format.uncolorize() : format.colorize(),
-                format.align(),
-                format.simple(),
-            )
-        })
+        new transports.Console()
     ]
 });
 
