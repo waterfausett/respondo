6f0feed331a0f879ddb6a454a37d2b899302dee1
waterfausett <waterfausett@gmail.com>
2020-07-04 01:31:27 -0500

    updated dm responses

--- app/message-handlers/direct-message.hanlder.js
+++ app/message-handlers/direct-message.hanlder.js
@@ -1,13 +1,16 @@
 const logger = require('../services/logger.service');
 const strings = require('../configuration/strings.json');
+const config = require('../configuration/bot.config.json');
 const BotMentionHandler = require('./bot-mention.handler');
 const CommandHandler = require('./command.handler');
 
 module.exports = {
     handleMessage: async (message) => {
         const results = await CommandHandler.handleMessage(message);
-        return results ||
-            BotMentionHandler.handleMessage('?')
-                .then(help => [[strings.idk_what_you_mean, ...help]]);
+        if (results) return results;
+        
+        return (message.toLowerCase() === 'help' || message.match(/^\?+$/))
+            ? BotMentionHandler.handleMessage('?')
+            : [[strings.idk_what_you_mean, `I can only process \`${process.env.command_prefix || config.commandPrefix}\` commands in DMs...\nwell, you can also ask for "help"`]];
     }
 }
